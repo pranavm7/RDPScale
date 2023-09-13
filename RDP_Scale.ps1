8e15dc3b11cd3b33de577c6bb17eb02b4518d68a
pranavm7 <pranavm7@outlook.com>
2023-09-12 20:06:53 -0400

    Checks if TailScale CLI is available

--- RDP_Scale.ps1
+++ RDP_Scale.ps1
@@ -1,43 +1,91 @@
-# Installing Chocolatey (To get the latest Tailscale version)
+#Requires -RunAsAdministrator
+
+# Change powershell terminal color from blue-yellow
+$Host.UI.RawUI.BackgroundColor = 'black'
+$Host.UI.RawUI.ForegroundColor = 'white'
 
 $script:executionPolicy = Get-ExecutionPolicy
+$script:tailscaleIP_Range= "100.64.0.0/10"
+
+# Checks for successful install of tailscale
+$script:tailscaleInstall = $False
+
+# Checks if tailscale CLI is available
+$script:tailscaleAvailable = $False
+
 
 if ($executionPolicy -eq "Restricted") {
 	Write-Host "Current execution policy is set to Restricted, recommended to change to AllSigned\nType: Set-ExecutionPolicy Bypass -Scope Process"
 	exit
 }
 
+# Check and install chocolatey package manager
 function Install-Chocolatey {
 	# Check if Chocolatey is installed
 	if (-not (Test-Path "$env:ProgramData\chocolatey\bin\choco.exe")) {
 	    
 	    # Chocolatey is not installed, so we go ahead and install it.
 	    # One liner from https://chocolatey.org/install#individual 
-	    Write-Host "Chocolatey is not installed. Installing Chocolatey..."
+	    Write-Host "Chocolatey is not installed.\n\nInstalling Chocolatey..."
 	    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 	
 	    # Check if Chocolatey installation was successful
 	    if (-not (Test-Path "$env:ProgramData\chocolatey\bin\choco.exe")) {
-	        Write-Host "Chocolatey installation failed. Exiting."
+	        Write-Host "\n[!]\tChocolatey installation failed. Exiting."
 	        exit 1
 	    }
 	    else {
-	        Write-Host "Chocolatey installed successfully."
+	        Write-Host "\n[+]\tChocolatey installed successfully.\n"
 	    }
 	}
-	else{ Write-Host "Chocolatey is installed! Proceeding to install TailScale..." }
+	else{ Write-Host "\n[.]\tChocolatey is installed! Proceeding to install TailScale...\n" }
 }
 
+# Install and check Tailscale installation
 function Install-Tailscale {
 	# Checks if tailscale is installed, if not, then installs tailscale using chocolatey.
 	if (-not(Test-Path -Path "$env:ProgramData\chocolatey\lib\tailscale")) {
 		choco install tailscale -y
+		# Check for successful install
+		if (-not(Test-Path -Path "$env:ProgramData\chocolatey\lib\tailscale")) {
+			Write-Host "\n[+]\tTailscale successfully installed!\n"
+			$tailscaleInstall=$True
+			}
+		else { 
+			Write-Host "\n\n[!]\tTailscale install failed.\nPlease try again using the following command:\n\t choco install tailscale -y\nAlternatively, install tailscale from https://tailscale.com/download/"
+			Write-Host "Exiting...\n"	
+				exit 1
+
+		     }
 	}
 	else {
-	    Write-Host "Tailscale is installed!"
+	    Write-Host "\n[+]\tTailscale is installed!"
 	}
+}
+
+# Check if the tailscale command exists
+
+function Check-TailscaleCli {
+	if (-not(Test-Path -Path (Get-Command tailscale -ErrorAction SilentlyContinue))){
+		$tailscaleAvailable=$True
+	}
+	else {
+		if($tailscaleInstall){
+			Write-Host "\n\n[!]\tThe tailscale command is not available but the application is installed.\n Please open the application and finish the setup, and run this script again!\n\nIf this error shows repeatedly, then a reboot is recommended.\n"
+			# exiting as 0 since it is not a script error, but pending configuration changes.
+			exit 0
+		}
 }
 
 Install-Chocolatey
 Install-Tailscale
+Check-TailscaleCli
 
+# Starts Tailscale
+if($(tailscale status) -like "*stop*" {
+	Write-Host "\n[+]\tRunning TailScale..."
+	tailscale up
+}
+
+# Prompt the user for client device alias
+$remoteClientName = Read-Host "\n[Action Required]\t"
